413ecd7795fb6dd0bf99293d7dfa118a58405081
JS0801 <jsuthar@oncloudconsulting.ca>
2026-07-27 13:34:19 -0400

    Update from NetSuite (Auto)

--- bc_timebill_automation_ue.js
+++ bc_timebill_automation_ue.js
@@ -583,6 +583,7 @@ define(['N/record', 'N/log', 'N/search'], function (record, log, search) {
     });
 
     setIfValue(salesOrder, 'entity', setupValues.customer);
+    log.debug('Customer Set',salesOrder.getValue('entity'));
     setIfValue(salesOrder, 'trandate', setupValues.trandate);
     setIfValue(salesOrder, 'subsidiary', setupValues.subsidiary);
     setIfValue(salesOrder, 'location', setupValues.location);
